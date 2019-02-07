4c68b8ba5f901c914e4be4adcb57b5080d4f72f7
Isaac Smith <isaac.smitty@gmail.com>
2019-02-07 17:45:11 +0000

    moved global var index into the function

--- findWaldo.js
+++ findWaldo.js
@@ -1,24 +1,18 @@
-var index;
-
-// The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
+
   arr.forEach(function(val,agg){
-  //for (var i = 0; i < arr.length; i++) {
-    if (val === "Waldo") {
-      index = agg;
-      found();   // execute callback
+
+  if (val === "Waldo") {
+    found(agg);   // execute callback
     }
   });
 }
 
-function actionWhenFound() {
-  console.log(`Found him at index ${index}.`);
+function actionWhenFound(agg) {
+  console.log(`Found him at index ${agg}.`);
 }
-function something (){
-  console.log('yah!')
-}
+// function something (){
+//   console.log('yah!')
+//}
 
-
-
-
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound;
